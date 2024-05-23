8806d976079bcb3d5783dc5707222d48f383bcb3
Hasafif <hassan.n.afif@gmail.com>
2024-05-23 06:24:33 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>HOME PAGE</h1>
-      <h5>This is the main directory route try #3 and it works!!</h5>
+      <h5>This is the main directory route try #7 and it  doest not work!!</h5>
     </main>
   );
 }
